547a4232daf212c4db26343ef9d31da854aa94d2
Çağatay Sarıoğlan <sarioglancgty@outlook.com>
2022-10-08 12:30:27 +0300

    Homework 3 project files created.

---
+++ Challenges/src/Challenge_03/MainClass.java
@@ -0,0 +1,14 @@
+package Challenge_03;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// T## 3.gün ödev
+
+		// 4 işlem yapan bir hesap makinesi yazılıp başka projelerde
+		// kütüphane olarak kullanılacak (main metodu olmayacak) 
+		// (jar dosyası olarak yazılacak)
+
+	}
+
+}
