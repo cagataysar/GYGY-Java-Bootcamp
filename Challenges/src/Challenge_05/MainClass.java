c4c549a794a7c41f4c38bdab6cce215ff8bce0ab
Çağatay Sarıoğlan <sarioglancgty@outlook.com>
2022-10-11 21:12:24 +0300

    New challenge added.

--- Challenges/src/Challenge_05/MainClass.java
+++ Challenges/src/Challenge_05/MainClass.java
@@ -3,7 +3,9 @@ package Challenge_05;
 public class MainClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Konsoldan girilen string içerisinde hangi harften kaç tane bulunduğunu sıralı
+		// !!! bir şekilde yazmak (Map kullanarak)
+		// Örnek: numan -> n2u1m1a1
 
 	}
 
